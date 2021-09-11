3796eb08cea93e608be3b0e2b4b42de5c4b85b89
Iradatul Wahdania <89880567+IRADATULWAHDANIA21@users.noreply.github.com>
2021-09-11 18:48:09 +0800

    Add files via upload

---
+++ halaman22.m
@@ -0,0 +1,20 @@
+% 1
+2/3.5, (3+5/4)^2
+((0.25)^2+(0.75)^2)^1/2, 2/(6/0.3)
+
+%2
+A=25, B=50, C=125, D=89
+X=A+B+C, Y=A/(D+B), Z=D^(A/B)+C
+
+%4
+x=pi/6 ; y=0.001
+sqrt (y),    exp (-x),    sin (x),     cos (2*x)
+tan (3 * x),   log10 (y),   log2 (y),    log (y)
+
+%5
+p=9+16*i, q=-9+16*i
+r=p*q,  s=p/q,  p-r
+r+s,    p^2
+sqrt (q),    abs (p),     angle (p),       abs (q)
+angle (q),   abs (r),     angle (r),       abs (s)
+angle (s)
